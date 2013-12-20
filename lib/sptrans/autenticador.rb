8a3edf8dfafc0138f2a23006da536de55ac5089d
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-12-20 17:24:47 -0200

    Corrige URL de autenticação

--- lib/sptrans/autenticador.rb
+++ lib/sptrans/autenticador.rb
@@ -5,7 +5,7 @@ module SPTrans
     end
 
     def autenticar! conexao
-      response = conexao.post "/v0/Login/Autenticar?token=#{ @token }"
+      response = conexao.post "Login/Autenticar?token=#{ @token }"
       conexao.autenticada = response.body == 'true'
 
       raise Excecoes::TokenInvalido unless conexao.autenticada?
