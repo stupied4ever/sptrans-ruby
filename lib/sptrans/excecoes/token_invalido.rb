b91fdcf4a678269c7d2a76efcd98f3458843efb5
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-12-20 16:37:49 -0200

    Mensagem de erro explicativa para token inválido

--- lib/sptrans/excecoes/token_invalido.rb
+++ lib/sptrans/excecoes/token_invalido.rb
@@ -2,7 +2,9 @@ module SPTrans
   module Excecoes
     class TokenInvalido < StandardError
       def initialize
-        super 'Token inválido'
+        super 'Token inválido, crie seu token em ' \
+              'http://www.sptrans.com.br/desenvolvedores/APIOlhoVivo/' \
+              'Documentacao.aspx'
       end
     end
   end
