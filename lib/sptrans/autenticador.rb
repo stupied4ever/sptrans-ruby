0f34e90fa21e5105d90d8fef523fe46a5bdcb1b6
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-12-20 17:04:21 -0200

    Autenticador#autenticar com token válido

--- lib/sptrans/autenticador.rb
+++ lib/sptrans/autenticador.rb
@@ -1,7 +1,14 @@
 module SPTrans
   class Autenticador
     def initialize token
-      raise Excecoes::TokenInvalido
+      @token = token
+    end
+
+    def autenticar conexao
+      response = conexao.post "/v0/Login/Autenticar?token=#{ @token }"
+      conexao.autorizado = response.body == 'true'
+
+      raise Excecoes::TokenInvalido unless conexao.autorizado?
     end
   end
 end
--- spec/sptrans/autenticador_spec.rb
+++ spec/sptrans/autenticador_spec.rb
@@ -9,8 +9,15 @@ module SPTrans
     describe '#autenticar' do
       subject(:autenticar) { autenticador.autenticar conexao }
 
-      context 'um token invalido' do
-        let(:conexao) { double(post: double(body: 'false')) }
+      let(:conexao) { Conexao.new conexao_faraday }
+      let(:conexao_faraday) { double(post: double(body: 'true')) }
+
+      it 'modifica o status da conexão para autorizada' do
+        expect { autenticar }.to change { conexao.autorizado? }.to(true)
+      end
+
+      context 'token inválido' do
+        let(:conexao_faraday) { double(post: double(body: 'false')) }
 
         it 'lança exceção TokenInvalido' do
           expect { autenticar }.to raise_error(Excecoes::TokenInvalido)
